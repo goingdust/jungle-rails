c3fdfe488885089667b133438a1ad3032c5ecb9d
goingdust <dustinluck@gmail.com>
2022-02-07 23:11:31 +0000

    Write specs for email that validates empty and not unique errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     before do
       @user1 = User.new(first_name: 'Tanjiro', last_name: 'Kamado', email: 'waterwheel@mail.com', password: 'nezuko',
                         password_confirmation: 'nezuko')
+      @user1.save
     end
 
     it 'should be saved successfully with all required fields set' do
@@ -27,6 +28,22 @@ RSpec.describe User, type: :model do
                           password: 'tanjiro', password_confirmation: 'tanjiro')
         @user2.save
         expect(@user2.errors.full_messages).to include("Last name can't be blank")
+      end
+    end
+
+    context 'email' do
+      it 'should include an error if empty' do
+        @user2 = User.new(first_name: 'Inosuke', last_name: 'Hashibara', password: 'tanjiro',
+                          password_confirmation: 'tanjiro')
+        @user2.save
+        expect(@user2.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'should include an error if email not unique (not case sensitive)' do
+        @user2 = User.new(first_name: 'Inosuke', last_name: 'Hashibara', email: 'WATERWHEEL@mail.com', password: 'tanjiro',
+                          password_confirmation: 'tanjiro')
+        @user2.save
+        expect(@user2.errors.full_messages).to include('Email has already been taken')
       end
     end
   end
