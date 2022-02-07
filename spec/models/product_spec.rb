911b5fa0c0614f5fc1d17250d05598bcd0e79ace
goingdust <dustinluck@gmail.com>
2022-02-07 20:02:34 +0000

    Write validation error specs for price, quantity, and category fields

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    
     before do
       @category = Category.new(id: 1, name: 'Electronics')
       @category.save
-      @product = Product.new(name: 'Smartwatch', price_cents: 100_000, quantity: 5, category_id: 1)
     end
 
     it 'should be saved successfully with all required fields set' do
+      @product = Product.new(name: 'Smartwatch', price_cents: 100_000, quantity: 5, category_id: 1)
       @product.save
-  
       expect(@product.id).to be_present
     end
 
@@ -24,7 +22,27 @@ RSpec.describe Product, type: :model do
     end
 
     context 'price' do
-      it 'should return an error if empty'
+      it 'should return an error if empty' do
+        @product = Product.new(name: 'Smartwatch', quantity: 5, category_id: 1)
+        @product.save
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+
+    context 'quantity' do
+      it 'should return an error if empty' do
+        @product = Product.new(name: 'Smartwatch', price_cents: 100_000, category_id: 1)
+        @product.save
+        expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      end
+    end
+
+    context 'category' do
+      it 'should return an error if empty' do
+        @product = Product.new(name: 'Smartwatch', price_cents: 100_000, quantity: 5)
+        @product.save
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
     end
   end
 end
