3bf2712b5642cadbdaafb3ed5f50896dbc110c38
goingdust <dustinluck@gmail.com>
2022-02-08 21:13:26 +0000

    Write spec that verifies the user is taken to product details when clicked on product

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Visitor navigates to the product detail page', type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |_n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'They see all product details' do
+    visit root_path
+
+    find('img', match: :first).click
+    sleep 5
+    save_screenshot
+
+    expect(page).to have_css 'article.product-detail'
+  end
 end
